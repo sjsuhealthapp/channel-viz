4fed23f1f0c3fea65357a22f332d3027c74bc000
sjsuhealthapp <jeff.mendoza12@gmail.com>
2014-03-29 15:24:26 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'rnlxXDkrIMhWlJVwP3es4j9I2uh4h1Meaj0d82JO6FfIoNOU', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1445399441], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
