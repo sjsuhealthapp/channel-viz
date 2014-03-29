f9e2623f0c7781298eafd6817058b1b61903f3fc
sjsuhealthapp <jeff.mendoza12@gmail.com>
2014-03-29 15:40:18 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,8 +14,8 @@
 
 	var defaultKey		= 'rnlxXDkrIMhWlJVwP3es4j9I2uh4h1Meaj0d82JO6FfIoNOU', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1445399441], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		applicationName	= 'SJSU Health App - Jeff M. and Shafayat H.', // Replaces Xively logo in the header
+		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
@@ -147,7 +147,7 @@
 									var graph = new Rickshaw.Graph( {
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
 										width: 600,
-										height: 200,
+										height: 600,
 										renderer: 'line',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
